f5dae8b2509ee14fc026214d21193789f2bc2eb1
R-tis <artis.ademi@gmail.com>
2023-04-06 22:30:57 +0200

    Scroll To Top implemented

--- view/src/App.js
+++ view/src/App.js
@@ -1,12 +1,25 @@
 import React from 'react'
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import {BrowserRouter, Routes, Route, useLocation} from 'react-router-dom';
 import {Box} from '@mui/material';
 import { Navbar, Feed, VideoDetail, ChannelDetail, SearchFeed } from './components';
+
+
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  
+  }, [pathname])
+  
+  return null;
+}
 
 const App = () => {
 
   return(
         <BrowserRouter>
+        <ScrollToTop />
         <Box sx={{backgroundColor: '#000'}}>
             <Navbar />
             <Routes>
